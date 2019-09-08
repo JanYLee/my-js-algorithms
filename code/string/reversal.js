aa9d5afead655a195a0d690116f445508632a998
DDLancer5 <694730359@qq.com>
2019-09-08 23:27:14 +0800

    feat: udpate# 更新反转字符串正则解法

--- code/string/reversal.js
+++ code/string/reversal.js
@@ -1,1 +1,5 @@
-export default (str) => (str.split(' ').map(item => item.split('').reverse().join('')).join(' '))
+// 拆分数组解法
+// export default (str) => (str.split(' ').map(item => item.split('').reverse().join('')).join(' '))
+
+// 正则
+export default (str) => (str.match(/[\w']+/g).map(item => item.split('').reverse().join('')).join(' '))
